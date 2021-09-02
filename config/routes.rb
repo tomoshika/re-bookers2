d67b5fe02c74daa7946b43c8da69d90f7aeaa7a6
tomoshika <tmm.orange.skm@gmail.com>
2021-09-02 04:22:44 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+  get 'homes/about'
+  
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
+  
+  devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
